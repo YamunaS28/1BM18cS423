63127c063700c3b20efa5ae07c0585c78d0e424f
YamunaS28 <53893224+YamunaS28@users.noreply.github.com>
2019-10-17 12:36:45 +0530

    Create prog7b.py

---
+++ prog7b.py
@@ -0,0 +1,18 @@
+def patern(n):
+    for i in range(n):
+        for j in range(n*(n-1)+1):
+            if(j%n!=0 and j!=n):
+                print("-",end="")
+            else:
+                print(" ",end="")
+        print('')
+        if i!=n-1:
+            for k in range(n*(n-1)+1):
+                if(k%n==0):
+                    print("|",end="")
+                else:
+                    print(" ",end="")
+            print("")
+k=int(input("enter num"))
+patern(k)
+    
